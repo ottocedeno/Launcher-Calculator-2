3e8f66a0401748a7969640e50ccb5a25a45b1b25
ottocedeno <otto@creativespaceclub.com>
2020-06-15 23:53:45 -0400

    added #lift_off_mass to rockets

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -5,4 +5,8 @@ class Rocket < ApplicationRecord
     stages = self.attributes.keys.find_all { |k| k.include?('stage_fuel') }
     stages.count { |s| self.send(s) != nil }
   end
+
+  def lift_off_mass
+    self.rocket_mass + self.fairing_mass + self.assumed_payload_mass
+  end
 end
