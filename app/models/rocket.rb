0c27dcac510ebcca33e10a80f71c5fe713a92e14
ottocedeno <otto@creativespaceclub.com>
2020-06-15 23:33:02 -0400

    #stages instance method added

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,2 +1,8 @@
 class Rocket < ApplicationRecord
+  include Stage
+
+  def stages
+    stages = self.attributes.keys.find_all { |k| k.include?('stage_fuel') }
+    stages.count { |s| self.send(s) != nil }
+  end
 end
