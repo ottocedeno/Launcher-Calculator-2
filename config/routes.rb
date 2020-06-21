455102591897b0799b4efc134b0663200dfb3262
ottocedeno <otto@creativespaceclub.com>
2020-06-20 23:04:06 -0400

    updated routes to include session :create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/login' => 'session#new', as: 'login'
+  get '/signup' => 'users#new', as: 'signup'
+  resources :session, only: [:create]
 end
