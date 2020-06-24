14f2b1aee349cf9f8cc60936b823a9cad93720b2
ottocedeno <otto@creativespaceclub.com>
2020-06-23 21:16:16 -0400

    updated User model to have validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :missions
   has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
