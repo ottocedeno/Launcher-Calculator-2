db2e9a1d0c22b420637806535a81c323df9c219d
ottocedeno <otto@creativespaceclub.com>
2020-07-08 08:51:21 -0400

    updated routes to include spaceport resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Login Flow
   root 'static#home'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   get '/login' => 'sessions#new', as: 'login'
@@ -8,8 +10,13 @@ Rails.application.routes.draw do
   get '/about' => 'static#about', as: 'about'
   resource :sessions, only: [:new, :create]
   resources :users, only: [:new, :create]
+
+  # Main Resources
   resources :rockets, only: [:index, :create, :show]
   patch '/rockets/:id' => 'rockets#update'
+
+  resources :spaceports, only: [:index]
+  
   resource :admin do
     resources :rockets, only: [:new, :edit, :destroy]
   end
