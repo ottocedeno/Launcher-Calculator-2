e43e06759b0cc558b67b0fbe99421236c002e683
ottocedeno <otto@creativespaceclub.com>
2020-07-08 20:13:40 -0400

    Spaceport Creation flow added

--- app/controllers/spaceports_controller.rb
+++ app/controllers/spaceports_controller.rb
@@ -1,5 +1,5 @@
 class SpaceportsController < ApplicationController
-  before_action :set_spaceport, only: [:show]
+  before_action :set_spaceport, only: [:show, :new]
 
   def index
     @spaceports = Spaceport.all
@@ -8,8 +8,30 @@ class SpaceportsController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create
+    @spaceport = Spaceport.new(spaceport_params)
+    if @spaceport.save
+      redirect_to spaceport_path(@spaceport)
+    else
+      render :new
+    end
+  end
+
   private
   def set_spaceport
     @spaceport = (Spaceport.find_by(id: params[:id]) || Spaceport.new)
+  end
+
+  def spaceport_params
+    params.require(:spaceport).permit(
+      :name,
+      :launch_point_altitude,
+      :spaceport_latitude,
+      :launch_azimuth,
+      :earth_rotation_velocity
+    )
   end
 end
