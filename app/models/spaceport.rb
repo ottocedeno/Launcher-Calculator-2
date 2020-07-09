fc136976122701bb916682428d4366f19f97bfa3
ottocedeno <otto@creativespaceclub.com>
2020-07-08 20:12:27 -0400

    Added validations to Spaceport Model

--- app/models/spaceport.rb
+++ app/models/spaceport.rb
@@ -1,6 +1,18 @@
 class Spaceport < ApplicationRecord
   has_many :missions
 
+  # Validations
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :launch_point_altitude, presence: true
+  validates :launch_point_altitude, numericality: {greater_than_or_equal_to: 0}
+  validates :spaceport_latitude, presence: true
+  validates :spaceport_latitude, numericality: {}
+  validates :launch_azimuth, presence: true
+  validates :launch_azimuth, numericality: {greater_than_or_equal_to: 0}
+  validates :earth_rotation_velocity, presence: true
+  validates :earth_rotation_velocity, numericality: {greater_than_or_equal_to: 0}
+
   def additional_velocity
     self.air_launch ? 250 : 0
   end
