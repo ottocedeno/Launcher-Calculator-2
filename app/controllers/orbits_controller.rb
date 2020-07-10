69c88a7a59714d4ccb26e8c257f57f4b2598dc3b
ottocedeno <otto@creativespaceclub.com>
2020-07-09 23:07:22 -0400

    added orbit show view

--- app/controllers/orbits_controller.rb
+++ app/controllers/orbits_controller.rb
@@ -1,4 +1,6 @@
 class OrbitsController < ApplicationController
+  before_action :set_orbit, only: [:show]
+  
   def index
     @orbits = Orbit.all
   end
@@ -10,5 +12,11 @@ class OrbitsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def set_orbit
+    @orbit = (Orbit.find_by(id: params[:id]) || Orbit.new)
   end
 end
--- app/helpers/orbits_helper.rb
+++ app/helpers/orbits_helper.rb
@@ -1,2 +1,5 @@
 module OrbitsHelper
+  def ommitted_orbit_keys(key)
+    ['id', 'name', 'quick_description', 'created_at', 'updated_at'].include?(key)
+  end
 end
