f0657c262e1cb63e05d3013ccfa83122f6ecaaeb
ottocedeno <otto@creativespaceclub.com>
2020-07-09 22:33:26 -0400

    added orbits routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,12 @@ Rails.application.routes.draw do
   resources :spaceports, only: [:index, :create, :show]
   patch '/spaceports/:id' => 'spaceports#update'
 
+  resources :orbits, only: [:index, :create, :show]
+  patch '/orbits/:id' => 'orbits#update'
+
   resource :admin do
     resources :rockets, only: [:new, :edit, :destroy]
     resources :spaceports, only: [:new, :edit, :destroy]
+    resources :orbits, only: [:new, :edit, :destroy]
   end
 end
