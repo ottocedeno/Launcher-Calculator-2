6bdb1f9dc965127f0647845ed3ffc830b0b515d5
ottocedeno <otto@creativespaceclub.com>
2020-07-10 23:02:33 -0400

    Added Editing functionality for Orbits

--- app/controllers/orbits_controller.rb
+++ app/controllers/orbits_controller.rb
@@ -1,11 +1,20 @@
 class OrbitsController < ApplicationController
-  before_action :set_orbit, only: [:show]
-  
+  before_action :set_orbit, only: [:show, :new, :edit, :update]
+
   def index
     @orbits = Orbit.all
   end
 
   def new
+  end
+
+  def create
+    @orbit = Orbit.new(orbit_params)
+    if @orbit.save
+      redirect_to orbit_path(@orbit)
+    else
+      render :new
+    end
   end
 
   def show
@@ -14,9 +23,31 @@ class OrbitsController < ApplicationController
   def edit
   end
 
+  def update
+    if @orbit.update(orbit_params)
+      redirect_to orbit_path(@orbit)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_orbit
     @orbit = (Orbit.find_by(id: params[:id]) || Orbit.new)
   end
+
+  def orbit_params
+    params.require(:orbit).permit(
+      :name,
+      :quick_description,
+      :orbit_perigee,
+      :orbit_apogee,
+      :orbit_inclination,
+      :orbital_period,
+      :delta_v,
+      :extra_velocity_to_planets
+    )
+  end
+  
 end
