a9e9bb224bdbb08710a8bf7859563f0964c67171
ottocedeno <otto@creativespaceclub.com>
2020-07-13 20:58:12 -0400

    mission #create action stubbed out

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -11,5 +11,30 @@ class MissionsController < ApplicationController
   end
 
   def new
+    @mission = Mission.new
+    load_mission_options
+  end
+
+  def create
+    @mission = Mission.new(mission_params)
+    if @mission.valid?
+      #@mission.mission_result = MissionResult.calculate
+      @mission.save
+      redirect_to user_mission_path(@mission, @mission.user_id)
+    else
+      load_mission_options
+      render :new
+    end
+  end
+
+  private
+  def mission_params
+    params.require(:mission).permit(:name, :user_id, :rocket_id, :spaceport_id, :orbit_id)
+  end
+
+  def load_mission_options
+    @rockets = Rocket.all
+    @spaceports = Spaceport.all
+    @orbits = Orbit.all
   end
 end
