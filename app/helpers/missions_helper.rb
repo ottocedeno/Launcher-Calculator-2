dd1c003229f46c989d177ecb02f62001f44368a5
ottocedeno <otto@creativespaceclub.com>
2020-07-15 18:55:07 -0400

    Stubbed out Mission show page + helper method for table

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -1,2 +1,5 @@
 module MissionsHelper
+  def ommitted_mission_keys(key)
+    ['id', 'name', 'user_id', 'rocket_id', 'spaceport_id', 'orbit_id', 'success', 'created_at', 'updated_at'].include?(key)
+  end
 end
